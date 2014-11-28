d97547709ffa9b73e26fa1a13d7776679e8fdb22
MattShearing <wickedmoments@hotmail.co.uk>
2014-11-28 17:00:21 +0000

    removed unneeded console.log used in testing

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,6 +1,5 @@
 function getTime() {
     var list = [];
-    console.log(player.getPlayerState():Number);
     var time = document.getElementById("video_time").innerHTML;
     list.push(time);
     showList(list);
