6a17840d2ec316ebf573936932ce03697677395c
MattShearing <wickedmoments@hotmail.co.uk>
2014-11-28 10:27:27 +0000

    updated folder structure for assets, js css videos

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,13 +1,13 @@
 function getTime() {
-	var list = array();
+    var list = array();
     var time = document.getElementById("video_time").innerHTML;
     list += time;
     showList(list);
 }
 function showList(list) {
     display = document.getElementById("list").innerHTML;
-    foreach (list as list_item) {
-        display = display + "<li>" + list_item + "</li>";
-	}
-	document.getElementById("list").innerHTML = display;
+    foreach (list_item in list) {
+        display += "<li>" + list_item + "</li>";
+    }
+    document.getElementById("list").innerHTML = display;
 }
