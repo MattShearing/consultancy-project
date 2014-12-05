6588ca45ec90bb902ed1b9caea4958bb0dd7e51c
mmu001 <markmuil87@gmail.com>
2014-12-05 10:01:11 +0000

    Added time function
    Added time function to convert seconds into a readable format.

--- submit.php
+++ submit.php
@@ -3,8 +3,29 @@
 //create array based on the data posted from previous page (clicks submitted) 
 $data = array();
 
+
+// create function to format time
+function formatSeconds( $seconds )
+{
+  $hours = 0;
+  $milliseconds = str_replace( "0.", '', $seconds - floor( $seconds ) );
+
+  if ( $seconds > 3600 )
+  {
+    $hours = floor( $seconds / 3600 );
+  }
+  $seconds = $seconds % 3600;
+
+
+  return str_pad( $hours, 2, '0', STR_PAD_LEFT )
+       . gmdate( ':i:s', $seconds, )
+       . ($milliseconds ? ".$milliseconds" : '')
+  ;
+}
+
 	foreach ($_POST as $posted)  {
-		$data[] = number_format($posted,2);
+		$data[] = formatSeconds( $posted);
+
 	}
 
 //print array and display array in a formatted structure 	
