17c6959ab8d22b091079a4805d5d06e23b2a573a
MattShearing <wickedmoments@hotmail.co.uk>
2014-12-12 15:26:14 +0000

    finished parameters template with javascript inputs

--- assets/js/function.js
+++ assets/js/function.js
@@ -11,3 +11,11 @@ function showList(list) {
     }
     document.getElementById("list").innerHTML = display;
 }
+function addNew() {
+	count = document.getElementById("count").innerHTML;
+	count++;
+	document.getElementById("count").innerHTML = count;
+	console.log(count);
+    newp = '<p>Point '+count+'<br><label>Start: </label><input type="text" id="start'+count+'" name="start'+count+'"><label>End: </label><input type="text" id="end'+count+'" name="end'+count+'"></p>';
+    document.getElementById("input_fields").innerHTML += newp;
+}
