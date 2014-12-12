30f6147a3f51f086068c52fcc10895db730b653b
MattShearing <wickedmoments@hotmail.co.uk>
2014-12-12 15:33:34 +0000

    removed unused older functions

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,16 +1,3 @@
-function getTime() {
-    var list = [];
-    var time = document.getElementById("video_time").innerHTML;
-    list.push(time);
-    showList(list);
-}
-function showList(list) {
-    display = document.getElementById("list").innerHTML;
-    for (index = 0; index < list.length; ++index) {
-        display += "<li>" + list[index] + "</li>";
-    }
-    document.getElementById("list").innerHTML = display;
-}
 function addNew() {
 	count = document.getElementById("count").innerHTML;
 	count++;
