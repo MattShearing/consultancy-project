acd3bf5036d3450943468c0a8b73cef7230006af
mmu001 <markmuil87@gmail.com>
2014-12-12 11:20:00 +0000

    Created css and linked stylesheet

--- listings.php
+++ listings.php
@@ -4,22 +4,28 @@
 ?>
 <html>
     <head>
+
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+
     </head>
     <body>
-        <ul>
-            <?php
-                while ($row = mysqli_fetch_array($result)) {
-                    if (strpos($row['videourl'], 'assets') !== false) {
-                        $url =  $row['videourl'];
-                    } else {
-                        $parts =  explode('=', $row['videourl']);
-                        $url = 'http://www.youtube.com/embed/' . $parts[1];
+        <div class="homebox">
+            <ul>
+                <?php
+                    while ($row = mysqli_fetch_array($result)) {
+                        if (strpos($row['videourl'], 'assets') !== false) {
+                            $url =  $row['videourl'];
+                        } else {
+                            $parts =  explode('=', $row['videourl']);
+                            $url = 'http://www.youtube.com/embed/' . $parts[1];
+                        }
+                ?>
+                        <a href="video.php?v=<?php echo $url; ?>"><li><?php echo $row['title']; ?></li></a>
+                <?php
                     }
-            ?>
-                    <a href="video.php?v=<?php echo $url; ?>"><li><?php echo $row['title']; ?></li></a>
-            <?php
-                }
-            ?>
-        </ul>
+                ?>
+            </ul>
+        <div>
     </body>
 </html>
+
