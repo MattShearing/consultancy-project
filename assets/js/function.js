4453f7ca7906ce06609d80e10cf8584eb6ab3eca
MattShearing <wickedmoments@hotmail.co.uk>
2015-01-23 10:35:46 +0000

    linked parameters page to account section

--- assets/js/function.js
+++ assets/js/function.js
@@ -3,6 +3,6 @@ function addNew() {
 	count++;
 	document.getElementById("count").innerHTML = count;
 	console.log(count);
-    newp = '<p>Point '+count+'<br><label>Start: </label><input type="text" id="start'+count+'" name="start'+count+'"><label>End: </label><input type="text" id="end'+count+'" name="end'+count+'"></p>';
+    newp = '<p>Point '+count+'<br><label>Start: </label><input type="text" id="start'+count+'" name="start'+count+'"><label> End: </label><input type="text" id="end'+count+'" name="end'+count+'"></p>';
     document.getElementById("input_fields").innerHTML += newp;
 }
--- myaccount.php
+++ myaccount.php
@@ -24,7 +24,7 @@
 
                 <!-- Teachers users can access the below -->
                 <li><a href="#">Upload Video</a></li>
-                <li><a href="#">Edit Video Parameters</a></li>
+                <li><a href="parameters.php">Edit Video Parameters</a></li>
                 <li><a href="register.php">Register Users</a></li>
                 <li><a href="#">Register Class Members</a></li>
                 <li><a href="#">Update Users</a></li>
