118abeaa26b4c6a7cb72fb58f8485e93da280126
mmu001 <markmuil87@gmail.com>
2015-01-23 11:46:30 +0000

    Added form validation to register user

--- assets/js/function.js
+++ assets/js/function.js
@@ -7,7 +7,7 @@ function addNew() {
     document.getElementById("input_fields").innerHTML += newp;
 }
 
-// login form validation 
+// Login form validation 
 $(window).load(function() {
 
  $('form#user-login').find('input[type="submit"]').click (function() {
@@ -24,6 +24,58 @@ $(window).load(function() {
       return false;
     } else {
       $('input#password').siblings('span.error').css("display","none");
+    }
+  });
+
+// Register form validation
+
+
+ $('form#user-register').find('input[type="submit"]').click (function() {
+    if ($('input#username').val().length == 0) {
+      $('input#username').siblings('span.error').css("display","block");
+      return false;
+    } else {
+      $('input#username').siblings('span.error').css("display","none");
+    }
+  });
+  $('form#user-register').find('input[type="submit"]').click (function() {
+    if ($('input#password').val().length == 0) {
+      $('input#password').siblings('span.error').css("display","block");
+      return false;
+    } else {
+      $('input#password').siblings('span.error').css("display","none");
+    }
+  });
+$('form#user-register').find('input[type="submit"]').click (function() {
+    if ($('input#fname').val().length == 0) {
+      $('input#fname').siblings('span.error').css("display","block");
+      return false;
+    } else {
+      $('input#fname').siblings('span.error').css("display","none");
+    }
+  });
+  $('form#user-register').find('input[type="submit"]').click (function() {
+    if ($('input#sname').val().length == 0) {
+      $('input#sname').siblings('span.error').css("display","block");
+      return false;
+    } else {
+      $('input#sname').siblings('span.error').css("display","none");
+    }
+  });
+$('form#user-register').find('input[type="submit"]').click (function() {
+    if ($('input#studentid').val().length == 0) {
+      $('input#studentid').siblings('span.error').css("display","block");
+      return false;
+    } else {
+      $('input#studentid').siblings('span.error').css("display","none");
+    }
+  });
+  $('form#user-register').find('input[type="submit"]').click (function() {
+    if ($('input#permission').val().length == 0) {
+      $('input#permission').siblings('span.error').css("display","block");
+      return false;
+    } else {
+      $('input#permission').siblings('span.error').css("display","none");
     }
   });
 
--- register.php
+++ register.php
@@ -2,7 +2,6 @@
 
 <?php
     require_once('header.php');
-    //$user = '';
 ?>
 
     <title>Home</title>
@@ -11,25 +10,37 @@
 <body>
         <div class="container">
                     
-	        <form action="user-register.php" method="POST">
+	        <form name="user-register" id="user-register" action="user-register.php" method="POST">
 
 	            <div class="register-box">
 	                
 	                <div>
-	                    <p><label for="username">Username:</label><input type="text" name="username" id="username" /></p>
+	                    <p><label for="username">Username:</label>
+	                    <span class="error">Required field</span><br>
+	                    <input type="text" name="username" id="username" required placeholder="Username"></p>
 	    
-	                    <p><label for="password">Password:</label><input type="password" name="password" id="password" /></p>
+	                    <p><label for="password">Password:</label>
+	                    <span class="error">Required field</span><br>
+	                    <input type="password" name="password" id="password" required placeholder="Password"></p>
 
-	                    <p><label for="fname">First Name:</label><input type="text" name="fname" id="fname" /></p>
+	                    <p><label for="fname">First Name:</label>
+	                    <span class="error">Required field</span><br>
+	                    <input type="text" name="fname" id="fname" required placeholder="First Name"></p>
 
-	                    <p><label for="sname">Last Name:</label><input type="text" name="sname" id="sname" /></p>
+	                    <p><label for="sname">Last Name:</label>
+	                    <span class="error">Required field</span><br>
+	                    <input type="text" name="sname" id="sname" required placeholder="Surname"></p>
 
-	                    <p><label for="studentid">Student ID:</label><input type="text" name="studentid" id="studentid" /></p>
+	                    <p><label for="studentid">Student ID:</label>
+	                    <span class="error">Required field</span><br>
+	                    <input type="text" name="studentid" id="studentid" required placeholder="Student ID"></p>
 
 	                    <!-- If is site admin permsission level visible, otherwise set default to 3(student) -->
 	                    <?php //echo $_SESSION['level'];exit; ?>
 	                    <?php if ($_SESSION['level'] == '1') {?>
-	                    	<p><label for="permission">Permission:</label><input type="text" name="permission" id="permission" /></p>
+	                    	<p><label for="permission">Permission:</label>
+	                    	<span class="error">Required field</span><br>
+	                    	<input type="text" name="permission" id="permission" required placeholder="Permission Level"></p>
 	                    <?php } else { ?>
 	                    	<input type="hidden" name="permission" id="permission" value="3" />
 	                    <?php } ?>
