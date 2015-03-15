c3a1391358df1bf300d868042f2ce955f0874f86
mmu001 <markmuil87@gmail.com>
2015-03-15 15:37:20 +0000

    documentation pages added

--- documentation/account/update.php
+++ documentation/account/update.php
@@ -9,6 +9,7 @@
 
     <body>
         <div class="container">
+        	<p>To update a users details, from the home screen select 'My Account' and select 'Update Users'. Here update the form, see below for example (create screen shot with example data). </p>
         	<a href="/documentation">Back to Documentation</a>
             <p>Video Documentation</p>
         </div>
--- documentation/account/user-class.php
+++ documentation/account/user-class.php
@@ -9,6 +9,7 @@
 
     <body>
         <div class="container">
+        	<p>To add a user to a class, from the home screen select 'My Account' and select 'Register Class Members'. Here complete the form, see below for example (create screen shot with example data).</p>
         	<a href="/documentation">Back to Documentation</a>
             <p>Video Documentation</p>
         </div>
